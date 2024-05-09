88c7427602c225976f6a384f1905f10654e1c1bc
agpavlik <agpavlik@gmail.com>
2024-05-08 22:55:53 -0400

    Add basic screens

--- App.js
+++ App.js
@@ -1,6 +1,10 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
+import ManageExpense from "./screens/ManageExpense";
+import RecentExpenses from "./screens/RecentExpenses";
+import AllExpenses from "./screens/AllExpenses";
+
 export default function App() {
   return (
     <View style={styles.container}>
